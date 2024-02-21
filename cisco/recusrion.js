32673317c0829260925341a486d85145827ac8e7
Gabogogi <bencitymaina@gmail.com>
2024-02-21 18:01:10 +0300

    recursion ternary operator

--- cisco/recusrion.js
+++ cisco/recusrion.js
@@ -1,2 +1,17 @@
 // A demonstration of recursion
 //Uses javascript for Web
+function recursion (n){
+    let result = 1;
+    while (n > 1){
+        result *= n;
+        n--;
+    }
+    return result;
+}
+console.log(recursion(6)); // 720
+
+// we can also use a ternary operator
+function ternaryRecursion(n){
+    return n > 1 ? n * ternaryRecursion(n -1): 1;
+}
+console.log(ternaryRecursion(6)); // 720
