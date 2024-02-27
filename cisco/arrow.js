ba2b4b91dd7db1c3b20ee0f0e3eb5e905b26c09e
Gabogogi <bencitymaina@gmail.com>
2024-02-27 23:53:20 +0300

    Arrow Parctice

---
+++ cisco/arrow.js
@@ -0,0 +1,15 @@
+// Before arrow functions
+hello =  function (){
+    return "hello World"
+}
+
+helloNew = () => {
+    return "Hellow World!"
+}
+
+// a step further
+
+helloNew2 = () => "Hello World!"
+
+console.log(helloNew())
+console.log(helloNew2())
