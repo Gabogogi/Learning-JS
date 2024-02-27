320cc1259974e8726fd123ca607a9804d673d22f
Gabogogi <bencitymaina@gmail.com>
2024-02-27 23:56:34 +0300

    arrow literals

--- cisco/arrow.js
+++ cisco/arrow.js
@@ -9,7 +9,12 @@ helloNew = () => {
 
 // a step further
 
-helloNew2 = () => "Hello World!"
+helloNew2 = () => "Hello World!" // only works if function has only one statement
 
 console.log(helloNew())
 console.log(helloNew2())
+
+// with parameters
+helloThere = (myName) => `My name is ${myName}`
+
+console.log(helloThere('John Doe'))
